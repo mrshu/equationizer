982c6fd4f954603fd5c66c71109637fd1fe5caff
mr.Shu <mr@shu.io>
2013-03-07 09:45:05 +0100

    few tests added

--- equationator.py
+++ equationator.py
@@ -1,1 +1,14 @@
 from sympy import solve
+
+res = solve('Eq(F, m*a)', 'a')
+print res
+
+
+res = solve('Eq(X, 1/2*a*t**2)', 't')
+print res
+
+res = solve('Eq(U, X*R)', 'R')
+print res
+
+# Sympy treats e, i and other important characters differently. We need to do
+# something about it.
